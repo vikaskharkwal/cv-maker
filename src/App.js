90a5cbc6a8ee3ac96630fd44578da713b39a0813
Vikas Kharkwal <kharkwalvikas@gmail.com>
2021-10-03 12:37:17 +0530

    converted to rfc and declared state using useState

--- src/App.js
+++ src/App.js
@@ -1,48 +1,39 @@
-import { Component } from 'react';
+import { useState } from 'react';
 // import './App.css';
 import CVForm from './components/CVForm';
 import CVPreview from './components/CVPreview';
 import Footer from './components/Footer';
 import Header from './components/Header';
 
-class App extends Component {
-  constructor(props) {
-    super(props);
+function App() {
+  const [general, setGeneral] = useState([
+    {
+      name: '',
+      github: '',
+      email: '',
+      facebook: '',
+      phone: '',
+      linkedin: '',
+      summary: '',
+    },
+  ]);
 
-    this.state = {
-      general: [
-        {
-          name: '',
-          github: '',
-          email: '',
-          facebook: '',
-          phone: '',
-          linkedin: '',
-          summary: '',
-        },
-      ],
-      degrees: [],
-      degree() {
-        return {
-          course: '',
-          institute: '',
-          from: '',
-          to: '',
-        };
-      },
-      jobs: [],
-      job() {
-        return {
-          position: '',
-          company: '',
-          from: '',
-          to: '',
-        };
-      },
-    };
-  }
+  const [degrees, setDegrees] = useState([]);
+  const [degree, setDegree] = useState({
+    course: '',
+    institute: '',
+    from: '',
+    to: '',
+  });
+  const [jobs, setJobs] = useState([]);
+  const [job, setJob] = useState({
+    position: '',
+    company: '',
+    from: '',
+    to: '',
+  });
 
-  addField = (formSet) => {
+  const addField = (formSet) => {
     this.setState((state) => {
       return {
         [formSet]: state[formSet].concat(
@@ -52,7 +43,7 @@ class App extends Component {
     });
   };
 
-  deleteField = (index, formSet) => {
+  const deleteField = (index, formSet) => {
     this.setState((state) => {
       return {
         [formSet]: state[formSet].filter((item, ind) => {
@@ -63,7 +54,7 @@ class App extends Component {
     console.log('deleting field');
   };
 
-  handleChange = (index, id, value, formSet) => {
+  const handleChange = (index, id, value, formSet) => {
     this.setState((state) => {
       return {
         [formSet]: state[formSet].map((item, ind) => {
@@ -76,37 +67,35 @@ class App extends Component {
     });
   };
 
-  render() {
-    return (
-      <div className='App'>
-        <Header />
-        <div className='formContainer'>
-          <h2>Form</h2>
-          <CVForm
-            degrees={this.state.degrees}
-            jobs={this.state.jobs}
-            general={this.state.general}
-            addField={this.addField}
-            deleteField={this.deleteField}
-            handleChange={this.handleChange}
-          />
-        </div>
-        <div className='previewPrint'>
-          <h2>Preview</h2>
-          <CVPreview
-            general={this.state.general}
-            degrees={this.state.degrees}
-            jobs={this.state.jobs}
-          />
-          <button onClick={window.print}>
-            <i className='fas fa-print'></i>
-            <span>Print</span>
-          </button>
-        </div>
-        <Footer />
+  return (
+    <div className='App'>
+      <Header />
+      <div className='formContainer'>
+        <h2>Form</h2>
+        <CVForm
+          degrees={this.state.degrees}
+          jobs={this.state.jobs}
+          general={this.state.general}
+          addField={this.addField}
+          deleteField={this.deleteField}
+          handleChange={this.handleChange}
+        />
       </div>
-    );
-  }
+      <div className='previewPrint'>
+        <h2>Preview</h2>
+        <CVPreview
+          general={this.state.general}
+          degrees={this.state.degrees}
+          jobs={this.state.jobs}
+        />
+        <button onClick={window.print}>
+          <i className='fas fa-print'></i>
+          <span>Print</span>
+        </button>
+      </div>
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
